3f22501ac79562df64a7bec55190c9c32f5263fa
Brendon Angelo Rodrigues <brendonangelo@hotmail.com>
2021-09-29 22:43:45 -0300

    chore: beginning of Todo List implementation

--- pages/index.vue
+++ pages/index.vue
@@ -1,77 +1,46 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p></p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <v-container fluid class="red pa-0 fill-height">
+    <v-row class="ma-0 d-flex fill-height justify-center">
+      <v-col cols="3" class="pa-0"
+        ><v-container class="blue darken-3 fill-height justify-center">
+          <v-card flat width="100%" height="80%" class="text-center">
+            <v-card-text>OIOI</v-card-text>
+          </v-card>
+        </v-container>
+      </v-col>
+      <v-col cols="6" class="pa-0">
+        <v-container class="pink fill-height justify-center">
+          <v-avatar color="blue" width="200" height="200">
+            <!-- <v-icon size="50%">mdi-account-circle</v-icon> -->
+          </v-avatar>
+        </v-container>
+      </v-col>
+      <v-col cols="3" class="pa-0">
+        <v-container class="blue darken-3 fill-height align-start">
+          <v-card flat width="100%" height="80%" class="">
+            <v-toolbar flat color="light-green accent-4">
+              <v-toolbar-title class="flex text-center">
+                Your Todo list
+              </v-toolbar-title>
+            </v-toolbar>
+            <v-text-field class=" pa-0 mt-0 ml-3 mr-3" />
+            <v-list two-line subheader> </v-list>
+          </v-card>
+        </v-container>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      todos: [],
+
+      newTodo: '',
+      step: '',
+    }
+  },
+})
+</script>
